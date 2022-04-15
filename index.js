02e798a68be4c9d1d2f38f2841603f2c35d28b6e
Decodedal <Palumbodallas@gmail.com>
2022-04-14 23:17:20 -0400

    trivia game is functional but needs work

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your Code Here
+// // Your Code Here
+// let username = window.prompt("please enter your name:")
+// let userscore = 0 
+
+// for (let i= 0; i < questions.length; i++){
+//     let question = questions[i]
+//     let userAnswer = window.prompt(question.text)
+// if (userAnswer === question.correctAnswer){
+//     userscore = userAnswer + 10
+// }
+// }
+// window.alert(username,"Your score is" +userscore)
+
+// let userName = window.prompt('Please enter your name:')
+let userScore = 0
+
+for(let i = 0; i < questions.length; i++){
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    if(userAnswer === question.correctAnswer){
+        userScore = userScore + 10
+    }
+}
+
+window.alert('Your score is: '+userScore)
+toup
